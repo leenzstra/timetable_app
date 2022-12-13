ab84674b1ac60445d5e751a17de592d0d380f176
SergeiNevolin <sergeinevolin9@gmail.com>
2022-12-13 17:57:20 +0500

    Доработал десериализатор

--- app/src/main/java/com/leenz/pnrpu/utils/JSONReader.java
+++ app/src/main/java/com/leenz/pnrpu/utils/JSONReader.java
@@ -2,9 +2,9 @@ package com.leenz.pnrpu.utils;
 
 import android.content.Context;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leenz.pnrpu.R;
 import com.leenz.pnrpu.models.Day;
-import com.leenz.pnrpu.models.Lesson;
 import com.leenz.pnrpu.models.Timetable;
 
 import org.json.JSONArray;
@@ -16,32 +16,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+
 public class JSONReader {
     public static Timetable readTimeTableJSONFile(Context context) throws IOException, JSONException {
         String jsonText = readText(context, R.raw.example_refactor);
 
         JSONObject jsonRoot = new JSONObject(jsonText);
         JSONArray data = jsonRoot.getJSONArray("data");
-        Day[] days = new Day[data.length()];
 
-        for (int i = 0; i < data.length();i++){
-            JSONObject obj = data.getJSONObject(i);
-            JSONArray table = obj.getJSONArray("table");
-            Lesson[] lessons = new Lesson[table.length()];
-            for (int j = 0; j < table.length(); j++) {
-                JSONObject subject = table.getJSONObject(j);
-                String time = subject.getString("time");
-                String subjectName = subject.getString("subject_name");
-                String subjectType = subject.getString("subject_type");
-                String teacher = subject.getString("teacher");
-                String location = subject.getString("location");
-                lessons[j] = new Lesson(time,subjectName,subjectType,teacher,location);
-            }
-            int id = obj.getInt("id");
-            int groupId = obj.getInt("group_id");
-            days[i] = new Day(id, groupId, obj.getString("day"), obj.getInt("week_num"),lessons);
-        }
-        return new Timetable(days);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Day[] daysArray = objectMapper.readValue(data.toString(), Day[].class);
+
+        return new Timetable(daysArray);
+
     }
 
     private static String readText(Context context, int resId) throws IOException{
